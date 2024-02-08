846b5861e61be5df390d676f6fa99d39d3412c3b
Erik <er.dijkstra@student.avans.nl>
2024-02-08 10:15:07 +0100

    Artist object aangemaakt

--- src/Data/Artist.java
+++ src/Data/Artist.java
@@ -1,4 +1,29 @@
 package Data;
 
+import java.awt.*;
+
 public class Artist {
+    private String name;
+    private String genre;
+
+    public Artist(String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
