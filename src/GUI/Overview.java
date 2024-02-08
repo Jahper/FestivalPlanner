f602426806dcfb3cddc7e2c1596c1cde00ed990b
Pieter <pieter.degraaff@icloud.com>
2024-02-08 12:44:19 +0100

    tableview aangemaakt

--- src/GUI/Overview.java
+++ src/GUI/Overview.java
@@ -7,6 +7,7 @@ public class Overview extends Tab {
     private Tab tab;
     public Overview(String name){
         Tab overview = new Tab(name);
+        Tableview tableview = new Tableview("tabelview");
         overview.setContent(new Label("hsbvjsjvsdfjbsfb"));
         this.tab = overview;
     }
