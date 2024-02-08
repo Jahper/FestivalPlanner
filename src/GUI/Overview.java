a1ff78a81131a59305a3dcfe6543d91fdd401883
Jahper <jaspertr04@gmail.com>
2024-02-08 12:44:24 +0100

    overview begin gemaakt

--- src/GUI/Overview.java
+++ src/GUI/Overview.java
@@ -1,15 +1,29 @@
 package GUI;
 
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+
+import java.awt.*;
 
 public class Overview extends Tab {
     private Tab tab;
     public Overview(String name){
         Tab overview = new Tab(name);
-        overview.setContent(new Label("hsbvjsjvsdfjbsfb"));
+        overview.setContent(getCanvas());
         this.tab = overview;
     }
+
+    private Node getCanvas() {
+
+
+
+
+
+        return new Canvas();//todo
+    }
+
     public Tab getTab(){
         return this.tab;
     }
