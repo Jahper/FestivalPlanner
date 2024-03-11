54be3f06fb4fab0958b6f27d173ff1ebe68583c4
Jahper <jaspertr04@gmail.com>
2024-03-11 09:14:16 +0100

    camera werkend en toegevoegd

--- src/GUI/Simulator/TiledMap.java
+++ src/GUI/Simulator/TiledMap.java
@@ -13,30 +13,36 @@ public class TiledMap {
     private Map map;
     private ResizableCanvas canvas;
     private BorderPane mainPane;
+    private FXGraphics2D g2d;
+    private Camera camera;
 
     public TiledMap() throws Exception {
         init();
         mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
-        FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        this.g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        this.camera = new Camera(canvas, g -> draw(g), g2d);
+        g2d.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
         draw(g2d);
     }
 
 
     public void init() {
         map = new Map("/GUI/Simulator/Festival_Planner_Lite_Version.json");
-
     }
 
 
     public void draw(Graphics2D g) {
+        g.setTransform(new AffineTransform());
+
         g.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-        map.draw(g);
+        g.setTransform(camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()));
         AffineTransform tx = new AffineTransform();
-        double zoom = canvas.getWidth() / 6000;
-        tx.scale(zoom, zoom);
-        g.setTransform(tx);
+//        double zoom = canvas.getWidth() / 6000;
+//        tx.scale(zoom, zoom);
+//        g.setTransform(tx);
+        map.draw(g);
     }
 
     public void update(double deltaTime) {
